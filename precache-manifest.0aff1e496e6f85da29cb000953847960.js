264e6be93ea26122ed33da1eb4caf598b5b0ba5c
JX <faishal@fiture.id>
2019-05-01 20:19:11 +0700

    Updates

---
+++ precache-manifest.0aff1e496e6f85da29cb000953847960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43d12b867c80f6dd113341bcff0ab5a",
+    "url": "/portfolio-landing/index.html"
+  },
+  {
+    "revision": "1eac7ba537f3cbb4a84c",
+    "url": "/portfolio-landing/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio-landing/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1eac7ba537f3cbb4a84c",
+    "url": "/portfolio-landing/static/js/main.04a493b8.chunk.js"
+  },
+  {
+    "revision": "8e1c4f83fb4e00735959",
+    "url": "/portfolio-landing/static/js/runtime~main.99036e1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-landing/static/media/logo.5d5d9eef.svg"
+  }
+]);
